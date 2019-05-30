07118f798617febfdc0f076bc8c0162ef0e508cb
Felipe Tuyama <felipetuyama@gmail.com>
2019-05-30 14:49:14 -0300

    Optimize headcount

--- headcount.rb
+++ headcount.rb
@@ -11,25 +11,31 @@ last_alive = @db.load()
 
 members = SlackCache.fetch(:users).map { |data| Member.new(data) }
 
-last_month = (Time.now - 86500*30).to_i
+last_month = (Time.now - 86400*30).to_i
 
 month_births = []
 month_deaths = []
 
 members.each do |member|
-  if member.updated > last_month
-    if member.deleted
-      month_deaths << member.profile_label
-    else
-      birth_time = member.birth_time
-      if birth_time && birth_time > last_month
-        month_births << member.profile_label
-      end
-    end
+  next if member.updated < last_month
+
+  if member.deleted
+    month_deaths << member.profile_label
+  else
+    picture_time = member.picture_time
+    next if picture_time.nil? || picture_time < last_month
+
+    birth_time = member.birth_time
+    next if birth_time.nil? || birth_time < last_month
+
+    month_births << member.profile_label
   end
 end
 
 binding.pry
-# FuneralCall.print_art(:grave)
-# FuneralCall.send_message("\n\n\t:skull: Members who died last month :skull:\n\n")
-# FuneralCall.send_message(month_deaths.join("\n"))
+FuneralCall.print_art(:grave)
+FuneralCall.send_message("\n\n\t[HEADCOUNT] ( *+#{month_births.count}* | *#{month_deaths.count}* )\n\n")
+FuneralCall.send_message("\n\n\t:skull: Members who died last month :skull:\n\n")
+FuneralCall.send_message(month_deaths.join("\n"))
+FuneralCall.send_message("\n\n\t:skull: Members who were born last month :skull:\n\n")
+FuneralCall.send_message(month_births.join("\n"))
--- model/member.rb
+++ model/member.rb
@@ -1,10 +1,11 @@
 class Member
-  attr_accessor :id, :name, :picture, :profile, :deleted, :updated, :data
+  attr_accessor :id, :name, :picture, :original_picture, :profile, :deleted, :updated, :data
 
   def initialize(data)
     @id = data["id"]
     @name = data["name"] || data["profile"]["real_name"] || data["real_name"]
     @title = data["profile"]["title"]
+    @original_picture = data["profile"]["image_original"]
     @picture = data["profile"]["image_192"]
     @deleted = data["deleted"]
     @updated = data["updated"]
@@ -13,6 +14,15 @@ class Member
 
   def death_time
     Time.at(@updated).strftime("%F %T")
+  end
+
+  def picture_time
+    begin
+      picture_time = @original_picture.scan(/(\d{4}-\d{2}-\d{2})/).last.first
+      Date.strptime(picture_time, "%Y-%m-%d").to_time.to_i
+    rescue
+      nil
+    end
   end
 
   def birth_time
